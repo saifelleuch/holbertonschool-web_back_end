d27ff030ab149ea21123110c8aa389e6dc1ffff3
saifelleuch <sa2014if@gmail.com>
2022-09-23 19:35:49 +0100

    another one

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""LRUCache that inherits from BaseCaching
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache that inherits from BaseCaching
+    """
+    def __init__(self):
+        super().__init__()
+        self.current_cache = []
+
+    def put(self, key, item):
+        """Must assign to the dictionary
+        self.cache_data the item value for the key key.
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self.current_cache:
+                self.current_cache.append(key)
+            else:
+                self.current_cache.append(
+                    self.current_cache.pop(self.current_cache.index(key)))
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                key_discarded = self.current_cache.pop(0)
+                del self.cache_data[key_discarded]
+                print("DISCARD: {}".format(key_discarded))
+
+    def get(self, key):
+        """Must return the value in
+        self.cache_data linked to key.
+        """
+        if key and key in self.cache_data:
+            self.current_cache.append(
+                self.current_cache.pop(self.current_cache.index(key)))
+            return self.cache_data.get(key)
+        return None
