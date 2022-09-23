154e6c84214011ad5a4e6f7350ebe8d1d1d5e30e
saifelleuch <sa2014if@gmail.com>
2022-09-23 16:54:53 +0100

    another one

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -14,11 +14,11 @@ class LIFOCache(BasicCaching):
         the item value for the key key.
         """
         len_stored_cache = len(self.cache_data)
+        lsc = len_stored_cache
         if key and item:
-            if len_stored_cache >= BasicCaching.MAX_ITEMS and str(
+            if lsc >= BasicCaching.MAX_ITEMS and str(
                     key) not in self.cache_data:
-                key_discarded = list(self.cache_data.keys())
-                [len_stored_cache - 1]
+                key_discarded = list(self.cache_data.keys())[lsc - 1]
                 print("DISCARD: {}".format(key_discarded))
                 self.cache_data.pop(key_discarded)
             self.cache_data[str(key)] = item
