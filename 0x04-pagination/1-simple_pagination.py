7d83508ce419926f1453cc1598633f7f2281ce3b
saifelleuch <sa2014if@gmail.com>
2022-09-27 02:13:42 +0100

    another one

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""1. Simple pagination
+"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """index_range that takes two
+    integer arguments page and page_size.
+    """
+    return (page * page_size - page_size, page * page_size)
+
+
+class Server:
+    """server class to paginate a
+    database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get_page that takes two integer
+        arguments page with default
+        value 1 and page_size with default value 10.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        first, last = index_range(page, page_size)
+        dataset = self.dataset()
+        if first > len(dataset):
+            return []
+        return self.dataset()[first:last]
