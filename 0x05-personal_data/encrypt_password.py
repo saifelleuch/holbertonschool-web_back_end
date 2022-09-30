935b4167d8a80ac53a0dbe63d081b9abc0dbec6b
saifelleuch <sa2014if@gmail.com>
2022-09-30 22:20:41 +0100

    another one

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -13,3 +13,10 @@ def hash_password(password: str) -> bytes:
     """
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """function that expects 2 arguments
+    and returns a boolean.
+    """
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
