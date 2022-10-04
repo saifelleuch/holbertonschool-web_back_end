b62d9853454ed6c714ae61abf8a10aa8fc3d39ce
saifelleuch <sa2014if@gmail.com>
2022-10-04 22:09:45 +0100

    another one

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,6 +10,8 @@ class BasicAuth(Auth):
     """ BasicAuthentication class that inherits from Auth"""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ method def extract_base64_authorization_header """
+
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
@@ -17,3 +19,5 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+
