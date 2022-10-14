57e018079447113c4f652b388a8714577abc141d
saifelleuch <sa2014if@gmail.com>
2022-10-14 20:34:16 +0100

    another one

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
-
-"""DB module
+"""
+DB model
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from user import Base, User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
-    """DB class
-    """
+    """ DB class """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+    def __init__(self):
+        """ Initialize a new DB instance """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ Memoized session object """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -61,13 +57,14 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        '''will update the user’s attributes as passed in
-        the method’s arguments then commit changes to the
-        database.'''
+        """This method takes in arbitrary keyword arguments
+        and returns the first row found in the users table
+        as filtered by the method’s input arguments.
+        """
         user = self.find_user_by(id=user_id)
-        for x, y in kwargs.items():
-            if not hasattr(user, x):
+        for k, v in kwargs.items():
+            if not hasattr(user, k):
                 raise ValueError
             else:
-                setattr(user, x, y)
+                setattr(user, k, v)
         self._session.commit()
