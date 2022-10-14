72d4fe9a1474ea1a8b0c50a7626dcd1d76640bf6
saifelleuch <sa2014if@gmail.com>
2022-10-14 18:18:33 +0100

    another one

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -59,3 +59,15 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        '''will update the user’s attributes as passed in
+        the method’s arguments then commit changes to the 
+        database.'''
+        user = self.find_user_by(id=user_id)
+        for x, y in kwargs.items():
+            if x not in User.__table__.columns.keys():
+                raise ValueError
+            else:
+                setattr(user, x, y)
+        self._session.commit()
