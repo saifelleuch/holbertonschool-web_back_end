c9d14f983c51e1ab1f7bc500422127a9994dc0ee
saifelleuch <sa2014if@gmail.com>
2022-10-14 19:36:21 +0100

    another one

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
 
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 
@@ -51,6 +51,19 @@ def login():
         return resp
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    """[Log out]
+    """
+    sidCocks = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(sidCocks)
+    if user is None:
+        abort(403)
+    else:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
 
 
 if __name__ == "__main__":
