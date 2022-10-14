21d4b7882cdf8c13565df1ff28aedabcfb435107
saifelleuch <sa2014if@gmail.com>
2022-10-14 20:02:53 +0100

    another one

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -79,3 +79,19 @@ def profile():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+
+
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """[reset password and get token]
+    """
+    email = request.form.get('email')
+
+    if email is None:
+        abort(403)
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except Exception as e:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": token}), 200
