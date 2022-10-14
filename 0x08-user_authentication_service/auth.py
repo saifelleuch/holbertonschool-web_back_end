6d91f2fa1f1dc1eefd8ccc66bbf65bb7c94702ae
saifelleuch <sa2014if@gmail.com>
2022-10-14 19:50:52 +0100

    another one

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -102,7 +102,7 @@ class Auth:
             return None
 
     def destroy_session(self, user_id: int) -> None:
-        """akes a single user_id integer argument and returns None.
+        """takes a single user_id integer argument and returns None.
         The method updates the corresponding user’s session ID to None.
         """
         if not user_id:
@@ -112,3 +112,18 @@ class Auth:
             return None
         except Exception as e:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ It take an email string
+        argument and returns a string.
+        """
+
+        if not email:
+            raise ValueError
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            self._db.update_user(user.id, reset_token=uuid)
+        except Exception as e:
+            raise ValueError
+        return uuid
