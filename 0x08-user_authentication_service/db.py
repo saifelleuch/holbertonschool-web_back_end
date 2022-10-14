45b868c7c7c95b865bc6a08ca1c60340b5e26680
saifelleuch <sa2014if@gmail.com>
2022-10-14 17:19:37 +0100

    another one

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -33,9 +33,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ save the user to the database """
-        user = User()
-        user.email = email
-        user.hashed_password = hashed_password
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
