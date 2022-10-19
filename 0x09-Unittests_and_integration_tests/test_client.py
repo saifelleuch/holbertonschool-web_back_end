3a7d06e2c2587dcbf9ee9040abd895792dad5f7a
saifelleuch <sa2014if@gmail.com>
2022-10-19 23:08:24 +0100

    another one

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -66,3 +66,13 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_json.assert_called_once()
     p = [({"license": {"key": "my_license"}}, "my_license", True),
          ({"license": {"key": "other_license"}}, "my_license", False)]
+
+    @parameterized.expand(p)
+    def test_has_license(self, mapping, license_key, excepted):
+        """
+        TestGithubOrgClient.test_has_license
+        to unit-test GithubOrgClient.has_license.
+        """
+
+        self.assertEqual(GithubOrgClient.has_license(
+            mapping, license_key), excepted)
