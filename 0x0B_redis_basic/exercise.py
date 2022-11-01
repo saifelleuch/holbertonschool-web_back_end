e21c6cf625622330c29183788a25c4ddfdde4a3d
saifelleuch <sa2014if@gmail.com>
2022-11-01 22:01:02 +0100

    another one

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """ 0x0B. Redis basic """
-
-from typing import Union, Optional, Callable
+from typing import Union, Callable, Optional, Any
 import redis
 import uuid
 from functools import wraps
 
 
-def call_history(Callable) -> Callable:
+def call_history(method: Callable) -> Callable:
     """store the history of inputs and outputs for a particular function"""
     key = method.__qualname__
     inputs = key + ":inputs"
@@ -23,7 +22,7 @@ def call_history(Callable) -> Callable:
     return wrapper
 
 
-def count_calls(Callable) -> Callable:
+def count_calls(method: Callable) -> Callable:
     """count for that key every time the method is called"""
     key = method.__qualname__
 
@@ -36,16 +35,16 @@ def count_calls(Callable) -> Callable:
 
 
 class Cache:
-    """ Cache class """
+    """ class """
     def __init__(self):
-        """ initialization of Cache class """
+        """initialization of Cache class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ method that takes a data argument and returns a string """
+        """method that takes a data argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -61,20 +60,21 @@ class Cache:
         return data
 
     def get_str(self, key: str) -> str:
-        """automatically parametrize Cache.get with
-        the correct conversion function."""
+        """ automatically parametrize Cache.get with
+        the correct conversion function. """
         data = self._redis.get(key)
         return data.decode("utf-8")
 
     def get_int(self, key: str) -> int:
-        """automatically parametrize Cache.get with
-        the correct conversion function."""
+        """ automatically parametrize Cache.get with
+        the correct conversion function. """
         data = self._redis.get(key)
         try:
             data = int(value.decode("utf-8"))
         except Exception:
             data = 0
         return data
+
 
 def replay(method: Callable):
     """ display the history of calls of a particular function """
