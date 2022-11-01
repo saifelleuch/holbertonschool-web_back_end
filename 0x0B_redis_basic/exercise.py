749dd11f965e65b74c12c8525e2ee305ad489a40
saifelleuch <sa2014if@gmail.com>
2022-11-01 11:23:28 +0100

    another one

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ 0x0B. Redis basic """
 
-from ctypes import Union
+from typing import Union, Optional, Callable
 import redis
 import uuid
 
@@ -19,3 +19,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """take a key string argument and an optional Callable
+        argument named fn. This callable will be used to
+        convert the data back to the desired format."""
+        data = self._redis.get(key)
+        if key is none:
+            return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """automatically parametrize Cache.get with
+        the correct conversion function."""
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """automatically parametrize Cache.get with
+        the correct conversion function."""
+        data = self._redis.get(key)
+        try:
+            data = int(value.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
